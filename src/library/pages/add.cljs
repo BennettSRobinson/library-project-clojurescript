4ba8bd7d3da7f28c39df2f06db036c51aabc5311
Bennett Robinson <bennett.robinson@techtonic.com>
2022-06-30 17:44:17 -0600

    implemented on change functionality for adding a book

--- src/library/pages/add.cljs
+++ src/library/pages/add.cljs
@@ -1,52 +1,71 @@
-(ns library.pages.add)
+(ns library.pages.add
+  (:require [reitit.frontend.easy :as rfe]
+            [reagent.core :as r]))
 
+(defn add! [event state]
+  (.preventDefault event)
+  (rfe/push-state :routes/bookshelf))
 
 (defn add-book
   []
-  [:main.mainAdd
-   [:h1.addTitle "Add Book"]
-   [:section.addBook
-    [:article.addBook__wrapper
-     [:form.addBook__forms
-      [:label.addBook__forms__wrapper
-       [:span.addBook__forms__title "Title"]
-       [:input.addBook__forms__input
-        {:id "title"
-         :type :text
-         :placeholder "title"}]]
-      [:label.addBook__forms__wrapper
-       [:span.addBook__forms__title "Author"]
-       [:input.addBook__forms__input--name
-        {:id "first"
-         :type :text
-         :placeholder "First Name...."}]
-       [:input.addBook__forms__input--name
-        {:id "last"
-         :type :text
-         :placeholder "Last Name...."}]]
-      [:label.addBook__forms__wrapper
-       [:span.addBook__forms__title "Summary"]
-       [:textarea.addBook__forms__input.addBook__forms__input--synopsis
-        {:id "summary"
-         :cols 50
-         :rows 10
-         :placeholder "Input a summary"}]]
-      [:div.addBook__forms__wrapper.addBook__forms__wrapper--published
-       [:label.addBook__forms__wrapper
-        [:span.addBook__forms__title "Published"]
-        [:input.addBook__forms__input.addBook__forms__input--calendar
-         {:id "published"
-          :type :text
-          :placeholder "MM/DD/YYYY"}]]
-       [:label.addBook__forms__wrapper
-        [:span.addBook__forms__title "Pages"]
-        [:input.addBook__forms__input.addBook__forms__input--calendar
-         {:id "pages"
-          :type :number
-          :placeholder 0}]]]]]
-    [:article.addBook__wrapper.addBook__wrapper--second
-     [:div.container
-      [:img.container__img {:src "/css/assets/pics/empty.jpeg"}]]]]
-   [:div.buttons-Wrapper
-    [:button.buttons-Wrapper__btns.buttons-Wrapper__btns--edit "Add Book"]
-    [:button.buttons-Wrapper__btns "Cancel"]]])
+  (let [initial-state {:title ""
+                       :author ""
+                       :summary ""
+                       :published ""
+                       :pages 0}
+        state (r/atom initial-state)]
+    (fn []
+      [:main.mainAdd
+       [:h1.addTitle "Add Book"]
+       [:section.addBook
+        [:article.addBook__wrapper
+         [:form.addBook__forms
+          [:label.addBook__forms__wrapper
+           [:span.addBook__forms__title "Title"]
+           [:input.addBook__forms__input
+            {:id "title"
+             :type :text
+             :on-change #(swap! state assoc :title (.. % -target -value))
+             :value (:title @state)
+             :placeholder "title"}]]
+          [:label.addBook__forms__wrapper
+           [:span.addBook__forms__title "Author"]
+           [:input.addBook__forms__input
+            {:id "author"
+             :type :text
+             :on-change #(swap! state assoc :author (.. % -target -value))
+             :value (:author @state)
+             :placeholder "Author...."}]]
+          [:label.addBook__forms__wrapper
+           [:span.addBook__forms__title "Summary"]
+           [:textarea.addBook__forms__input.addBook__forms__input--synopsis
+            {:id "summary"
+             :cols 50
+             :rows 10
+             :value (:summary @state)
+             :on-change #(swap! state assoc :summary (.. % -target -value))
+             :placeholder "Input a summary"}]]
+          [:div.addBook__forms__wrapper.addBook__forms__wrapper--published
+           [:label.addBook__forms__wrapper
+            [:span.addBook__forms__title "Published"]
+            [:input.addBook__forms__input.addBook__forms__input--calendar
+             {:id "published"
+              :type :text
+              :value (:published @state)
+              :on-change #(swap! state assoc :published (.. % -target -value))
+              :placeholder "MM/DD/YYYY"}]]
+           [:label.addBook__forms__wrapper
+            [:span.addBook__forms__title "Pages"]
+            [:input.addBook__forms__input.addBook__forms__input--calendar
+             {:id "pages"
+              :type :number
+              :on-change #(swap! state assoc :pages (.. % -target -value))
+              :value (:pages @state)
+              :placeholder 0}]]]]]
+        [:article.addBook__wrapper.addBook__wrapper--second
+         [:div.container
+          [:img.container__img {:src "/css/assets/pics/empty.jpeg"}]]]]
+       [:div.buttons-Wrapper
+        [:button.buttons-Wrapper__btns.buttons-Wrapper__btns--edit {:type :submit
+                                                                    :on-click #(add! % @state) } "Add Book"]
+        [:button.buttons-Wrapper__btns {:on-click #(rfe/push-state :routes/bookshelf)}"Cancel"]]])))
