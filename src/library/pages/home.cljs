01eb3bc82f062060f006f47df357ac644f4d0821
Bennett Robinson <bennett.robinson@techtonic.com>
2022-06-30 16:54:44 -0600

    add button routing on home page

--- src/library/pages/home.cljs
+++ src/library/pages/home.cljs
@@ -1,4 +1,5 @@
-(ns library.pages.home)
+(ns library.pages.home
+  (:require [reitit.frontend.easy :as rfe]))
 
 (defn home
   []
@@ -10,8 +11,8 @@
     [:p.mainHome__title "Welcome to the library"]
     [:p.mainHome__description "This is the greatest book in the world you should
 definiety come pick it up."]
-    [:button.btn "See Books"]
+    [:button.btn {:on-click #(rfe/push-state :routes/bookshelf)} "See Books"]
     [:p.mainHome__description "This is a very long text that you should look at
 if you want to know what is going on. This project wants me to write some text
 about some books"]
-    [:button.btn "Add a book"]]])
+    [:button.btn {:on-click #(rfe/push-state :routes/add)} "Add a book"]]])
